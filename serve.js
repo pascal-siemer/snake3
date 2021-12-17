d1f01593189d533ce71e86e4ac0622d8503411c1
pascal-siemer <95932011+pascal-siemer@users.noreply.github.com>
2021-12-17 02:00:32 +0100

    Add files via upload

---
+++ serve.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+const port = 1234;
+http.createServer(function(request, response) {
+    if(request.url === "/snake.js") {
+        response.writeHead(200, {'Content-Type': 'text/javascript'});
+        fs.createReadStream('snake.js').pipe(response);
+    } else if(request.url === "/apple.png") {
+        response.writeHead(200, {'Content-Type': 'image/png'});
+        fs.createReadStream('apple.png').pipe(response);
+    } else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream('index.html').pipe(response);
+    }
+}).listen(port);
+
+//serve.js sorgt dafür, dass ein Webserver gestartet wird auf Grundlage der Dateien index.html und snake.js
+//Port ist 1234
